bb1793ea00103748db5e5a67917b052d96abd5cd
Toprak Doğan <seyahdoo@gmail.com>
2026-06-12 14:47:17 +0300

    boop

--- main.py
+++ main.py
@@ -2,22 +2,31 @@
 import subprocess
 import argparse
 import json
+from unittest import case
 
 def main():
     parser = argparse.ArgumentParser(
         prog='Yo Git Tools',
         description='Simplified git process tools')
-    parser.add_argument('command')
+    parser.add_argument('command', 
+                        choices=['nuke', 'new', 'fetch', "pull", "push", "pr", "merge"])
     args = parser.parse_args()
-
     settings = load_settings()
-    
-    if args.command == "nuke":
-        return nuke()
-    
-    if args.command == "new":
-        return new(settings, args)
-
+    match args.command:
+        case "nuke":
+            return nuke()
+        case "new":
+            return new(settings, args)
+        case "fetch":
+            return fetch()
+        case "pull":
+            return pull()
+        case "push":
+            return push()
+        case "pr":
+            return pr()
+        case "merge":
+            return merge()
     return 1
 
 def load_settings():
@@ -33,8 +42,31 @@ def new(settings, args):
     run(["git", "stash", "push", "--include-untracked"])
     run(["git", "fetch", "--prune"])
     run(["git", "switch", f"origin/{settings["default-branch"]}", "--detach"])
-    result = run(["git", "switch", "-c", args.argument_one])
-    return result
+    return run(["git", "switch", "-c", args.argument_one])
+
+def fetch(settings):
+    run(["git", "fetch", "--prune", "--progress"])
+    return run(["git", "lfs", "fetch", "origin", settings["default-branch"]])
+
+def pull():
+    # foreach local branch merge latest develop
+    print("not implemented yet")
+    return 1
+
+def push():
+    run(["git", "add", "."])
+    run(["git", "commit", "-a", "--allow-empty-message", "-m", "\'\'"])
+    return run(["git", "push"])
+
+def pr():
+    print("not implemented yet")
+    return 1
+
+
+def merge():
+    print("not implemented yet")
+    return 1
+
 
 def run(arr):
     print(arr)
