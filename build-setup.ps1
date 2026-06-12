543e6ddc5b05c9df5ba34455826c45ceeb64cd66
Toprak Doğan <seyahdoo@gmail.com>
2026-06-12 13:47:10 +0300

    initial commit

---
+++ build-setup.ps1
@@ -0,0 +1,7 @@
+﻿
+Remove-Item -Path "dist" -Recurse
+Remove-Item -Path "setup\Output" -Recurse
+
+pyinstaller --onefile main.py --name yo
+
+setup\InnoSetup6\iscc.exe setup\setup-script.iss
---
+++ compile.ps1
@@ -0,0 +1,5 @@
+﻿
+Remove-Item -Path "dist" -Recurse
+
+pyinstaller --onefile main.py --name yo
+
